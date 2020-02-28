83436ebbfb05510e35fc9e893c2b17f21d048454
Arati Acharya <arati.acharya.2023@student.lhschools.org>
2020-02-28 12:01:04 -0500

    intelij commits

---
+++ src/ErrorCheckQuiz1/src/com/company/CP1ErrorCheckQuiz.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class CP1ErrorCheckQuiz {
+
+    public static void main(String[] args) {
+        //TODO find all 10 runtime errors
+        //TODO correct those errors
+        //TODO take a screen shot of the console showing that you cleared all the errors or which ones remain
+        //TODO attach screenshot and your edited "CP1ErrorCheckQuiz.java" file to the Google form
+        //TODO submit by the deadline
+        String typesOfErrors = "runtime";
+        System.out.println("This is a quiz that is to test your ability to find and fix "+ typesOfErrors);
+        System.out.println("In class we talked about "+ typesOfErrors);
+        System.out.println("We mentioned how these are the errors");
+        System.out.println("That will not allow a program to run without first fixing");
+        System.out.print("We diagrammed the different types of "+ typesOfErrors);
+        System.out.println("We also discussed solutions for fixing each type of " + typesOfErrors + " errors");
+        System.out.println("You all have been practicing fixing errors that you have created test questions with to help");
+        System.out.print("prepare you for this quiz");
+        System.out.println("This quiz should be a breeze");
+        System.out.println("For obvious reasons, I am not stating all of the different types of errors");
+        System.out.println("However, you should still be able to find and fix all of them to get an easy 100%");
+        System.out.println();
+        System.out.println("Goodluck, and may the odds ever be in your favor");
+
+
+    }
+}
---
+++ src/src/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Instruction for project
+//        comment
+//        System.out.println("Need to have a boarder around the receipt ");
+//        System.out.println("Need to have Gas Station name");
+//        System.out.println("Need to have the address");
+//        System.out.println("Need to have the date(same line)");
+//        System.out.println("Need to have the time(same line)");
+//        System.out.println("Need to have invoice number");
+//        System.out.println("Need to have authorization");
+//        System.out.println("Need to have the pump, product, $/g (same line)");
+//        System.out.println("Need to have the amount and the fuel total(same line)");
+//        System.out.println("Need to have a Thank you from the gas ");
+
+        System.out.println("-----------------------");
+        System.out.println("|      GAS STATION     |");
+        System.out.println("|         BRAND        |");
+        System.out.println("|      SALE RECEIPT    |");
+        System.out.println("|                      |");
+        System.out.println("|   8861 E Broad St,   |");
+        System.out.println("| Reynoldsburg,OH,43068|");
+        System.out.println("|                      |");
+        System.out.println("|                      |");
+        System.out.println("| DATE           TIME  |");
+        System.out.println("| 01/22/2020    4:01PM |");
+        System.out.println("| INVOICE#    10101010 |");
+        System.out.println("|       MASTERCARD     |");
+        System.out.println("|      ACCOUNT NUMBER  |");
+        System.out.println("|    12345678910119    |");
+        System.out.println("|                      |");
+        System.out.println("| PUMP  PRODUCT  $/G   |");
+        System.out.println("|  01     UNLD   $2.9  |");
+        System.out.println("|                      |");
+        System.out.println("| GALLONS  FUEL  TOTAL |");
+        System.out.println("   20.69      $   60.00|");
+        System.out.println("|                      |");
+        System.out.println("|      THANK YOU!      |");
+        System.out.println("|      GAS STATION     |");
+        System.out.println("|        BRAND         |");
+        System.out.println("|----------------------|");
+
+    }
+}
